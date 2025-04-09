0b91056edc25fcb6b834ab49d1f61a3314b5b077
gawasa29 <yusei112929@gmail.com>
2025-04-09 20:56:27 +0900

    add Logger

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+import { Logger } from "./services/logging/Logger"
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let outputChannel: vscode.OutputChannel
+
 export function activate(context: vscode.ExtensionContext) {
+	outputChannel = vscode.window.createOutputChannel("Cline")
+	context.subscriptions.push(outputChannel)
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "aichat" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('aichat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from aichat!');
-	});
-
-	context.subscriptions.push(disposable);
+	Logger.initialize(outputChannel)
+	Logger.log("AICHAT extension activated")
+	outputChannel.show()
 }
 
 // This method is called when your extension is deactivated
