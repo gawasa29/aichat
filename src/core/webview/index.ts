42c684253ebd78a1aea70dbae06d8a1c9f4f4b74
gawasa29 <yusei112929@gmail.com>
2025-04-09 21:48:52 +0900

    add sidebar

---
+++ src/core/webview/index.ts
@@ -0,0 +1,74 @@
+import * as vscode from "vscode"
+import { getNonce } from "./getNonce"
+
+export class WebviewProvider implements vscode.WebviewViewProvider {
+	public static readonly sideBarId = "aichat-dev.SidebarProvider" // used in package.json as the view's id. This value cannot be changed due to how vscode caches views based on their id, and updating the id would break existing instances of the extension.
+	private static activeInstances: Set<WebviewProvider> = new Set()
+	public view?: vscode.WebviewView | vscode.WebviewPanel
+
+	constructor(
+		readonly context: vscode.ExtensionContext,
+
+		private readonly outputChannel: vscode.OutputChannel
+	) {
+		WebviewProvider.activeInstances.add(this)
+	}
+
+	async resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		token: vscode.CancellationToken
+	) {
+		this.view = webviewView
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+			localResourceRoots: [this.context.extensionUri],
+		}
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "main.js"))
+
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "reset.css"))
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "vscode.css"))
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "main.css"))
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce()
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+
+				<button class="add-color-button">Add Color</button>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,22 @@
 import * as vscode from "vscode"
+import { WebviewProvider } from "./core/webview"
 import { Logger } from "./services/logging/Logger"
+
+/*
+Built using by
+https://github.com/microsoft/vscode-webview-ui-toolkit
+https://github.com/cline/cline
+
+Inspired by
+https://github.com/microsoft/vscode-webview-ui-toolkit-samples/tree/main/default/weather-webview
+https://github.com/microsoft/vscode-webview-ui-toolkit-samples/tree/main/frameworks/hello-world-react-cra
+
+*/
 
 let outputChannel: vscode.OutputChannel
 
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	outputChannel = vscode.window.createOutputChannel("Cline")
 	context.subscriptions.push(outputChannel)
@@ -10,6 +24,14 @@ export function activate(context: vscode.ExtensionContext) {
 	Logger.initialize(outputChannel)
 	Logger.log("AICHAT extension activated")
 	outputChannel.show()
+
+	const sidebarWebview = new WebviewProvider(context, outputChannel)
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(WebviewProvider.sideBarId, sidebarWebview, {
+			webviewOptions: { retainContextWhenHidden: true },
+		})
+	)
 }
 
 // This method is called when your extension is deactivated
