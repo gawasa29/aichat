4c85d43976b196187ec344c5710004755ad258fe
gawasa29 <yusei112929@gmail.com>
2025-04-13 00:24:54 +0900

    a

--- webview-ui/src/components/chat/ChatView.tsx
+++ webview-ui/src/components/chat/ChatView.tsx
@@ -3,9 +3,6 @@ const ChatView = () => {
 		<div>
 			<div className="flex flex-col h-full">
 				<div className="flex flex-row flex-1">
-					<div className="flex-none w-1/4 bg-gray-200 p-4">
-						<p>Sidebar</p>
-					</div>
 					<div className="flex-1 bg-white p-4">
 						<p>Main Chat Area</p>
 					</div>
